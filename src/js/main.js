a79d9ffc83e796d652e2f48ecf190c3aa58dd552
Nguyên Trần <nguyentranthepog@gmail.com>
2021-06-18 14:34:48 +0700

    feat: header

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,14 @@
 const menuToggle = document.querySelector('.header__toggle');
 const header = document.querySelector('.header');
 const headerOverlay = document.querySelector('.header__overlay');
+const body = document.querySelector('body');
 
 menuToggle.addEventListener('click', () => {
-  header.classList.toggle('active');
+  header.classList.add('active');
+  body.style.overflowY = 'hidden';
 });
 
 headerOverlay.addEventListener('click', () => {
   header.classList.remove('active');
+  body.style.overflow = 'auto';
 });
