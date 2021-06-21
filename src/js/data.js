65d308ed981ba29ae57e4ad736f8957b3a286e76
Nguyên Trần <nguyentranthepog@gmail.com>
2021-06-21 16:59:21 +0700

    chore: refactor feature list

---
+++ src/js/data.js
@@ -0,0 +1,47 @@
+import lock from 'url:../images/lock.svg';
+import fire from 'url:../images/fire.svg';
+import puzzle from 'url:../images/puzzle.svg';
+import people from 'url:../images/people.svg';
+import user from 'url:../images/user.svg';
+import mail from 'url:../images/mail-red.svg';
+
+const data = [
+  {
+    icon: lock,
+    title: 'Security Management',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    featureLink: '#',
+  },
+  {
+    icon: fire,
+    title: 'Digital Agency',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    link: '#',
+  },
+  {
+    icon: puzzle,
+    title: 'API Reference',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    link: '#',
+  },
+  {
+    icon: people,
+    title: 'Team Accounts',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    link: '#',
+  },
+  {
+    icon: user,
+    title: 'User Administration',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    link: '#',
+  },
+  {
+    icon: mail,
+    title: 'Email Marketing',
+    desc: 'It is a long established fact that a reader will distracted by the readable content',
+    link: '#',
+  },
+];
+
+export default data;
